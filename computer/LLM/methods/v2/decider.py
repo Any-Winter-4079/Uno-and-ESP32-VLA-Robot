aac9f07248821fea3e55c5dee58bdf678b5e101b
Any-Winter-4079 <50542132+Any-Winter-4079@users.noreply.github.com>
2025-11-09 21:53:23 +0100

    add prompting methods (from prompting experiments)

---
+++ computer/LLM/methods/v2/decider.py
@@ -0,0 +1,13 @@
+from utils.v2.utils import run_llm, extract_and_format_value
+
+def run_decider(question, exemplars):
+    message = f"{exemplars}\nQ: {question}\n\nA: "
+    prediction = run_llm(message, ["Q:"], decider=True)[0]
+    predicted_answer = prediction.split("The answer is ")[-1].strip()
+    predicted_answer = extract_and_format_value(predicted_answer)
+    print("\tDecider:", predicted_answer)
+    return {
+        "message": message,
+        "prediction": prediction,
+        "concise_prediction": predicted_answer
+    }
---
+++ computer/LLM/methods/v3/chain_of_thought.py
@@ -0,0 +1,13 @@
+from config.v3.config import MODEL_TEMPLATE, STOP
+from utils.v3.utils import run_llm, extract_and_format_value
+
+def run_cot(question, exemplars, cpp=False, iteration=0):
+    user_end = MODEL_TEMPLATE["user_end"]
+    assistant_start = MODEL_TEMPLATE["assistant_start"]
+    message = f"{exemplars}{question}{user_end}\n{assistant_start}"
+    prediction = run_llm(message, STOP, cpp=cpp, iteration=iteration)
+    concise_prediction = prediction.split("The answer is ")[-1].strip()
+    concise_prediction = extract_and_format_value(concise_prediction)
+    prediction = f"{prediction}\n#### {concise_prediction}"
+    print("\tCoT:", concise_prediction)
+    return message, prediction, concise_prediction
